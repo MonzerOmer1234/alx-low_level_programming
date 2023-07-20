9b2dbbdf7badbf9003cc15828af65f0de5b6af0b
Monzer Abdelgafar <monzeromer048@gmail.com>
2023-07-20 06:04:08 -0700

    m

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,10 @@
  *
  * Return: 0 or 1
  */
-int _isupper(int x)
+int _isupper(int c)
 {
    
-       	if (x >= 65 && x <= 90)
+       	if (c >= 65 && c <= 90)
 	{
                  return (1) ;
 	}  
